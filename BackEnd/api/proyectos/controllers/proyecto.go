9f9b1a3ed22e8183ac248b7b0b4485070896a603
fdrossi <fdrossi24@gmail.com>
2023-06-29 20:43:52 -0300

    Rutas proyecto, BBDD y pruebas con RestClient

--- BackEnd/api/proyectos/controllers/proyecto.go
+++ BackEnd/api/proyectos/controllers/proyecto.go
@@ -5,7 +5,10 @@ import (
 	"ASTRIC/BackEnd/shared/db"
 	"ASTRIC/BackEnd/shared/ep"
 	"encoding/json"
+
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func CrearProyecto(w http.ResponseWriter, r *http.Request) {
@@ -32,9 +35,70 @@ func CrearProyecto(w http.ResponseWriter, r *http.Request) {
 
 	if result.RowsAffected < 1 {
 		res.Err("No se puedo crear el proyecto.").DatoSend(proyecto)
-
+		return
 	}
 
 	res.DatoSend(proyecto)
 	defer cancel()
+}
+
+func EliminarProyecto(w http.ResponseWriter, r *http.Request) {
+	defer ep.ErrorControlResponse("Proyecto/EliminarProyecto", w, r)
+	res := ep.NewResponse("Eliminar Proyecto", w)
+
+	params := mux.Vars(r)
+
+	conexion, cancel := db.MysqlORM()
+
+	result := conexion.Delete(&models.Proyecto{}, params["id"])
+
+	if result.Error != nil {
+		res.Err("No se puede eliminar el Proyecto")
+		return
+	}
+
+	defer cancel()
+}
+
+func ObtenerProyectos(w http.ResponseWriter, r *http.Request) {
+	defer ep.ErrorControlResponse("Proyecto/ObtenerProyectos", w, r)
+	res := ep.NewResponse("Obtener Proyectos", w)
+
+	var proyectos []models.Proyecto
+
+	conexion, cancel := db.MysqlORM()
+
+	result := conexion.Find(&proyectos)
+
+	if result.Error != nil {
+		res.Err("No se pudiern obtener proyectos")
+		return
+	}
+
+	defer cancel()
+
+	json.NewEncoder(w).Encode(proyectos)
+}
+
+func ModificarProyecto(w http.ResponseWriter, r *http.Request) {
+	defer ep.ErrorControlResponse("Proyecto/ObtenerProyectos", w, r)
+	res := ep.NewResponse("Obtener Proyectos", w)
+	var proyecto models.Proyecto
+
+	err := json.NewDecoder(r.Body).Decode(&proyecto)
+	if err != nil {
+		res.ErrSend(err.Error())
+		return
+	}
+
+	conexion, cancel := db.MysqlORM()
+	defer cancel()
+
+	result := conexion.Updates(&proyecto)
+
+	if result.RowsAffected < 1 {
+		res.Err("No se pudo actualizar el proyecto").DatoSend(proyecto)
+	}
+
+	res.DatoSend(proyecto)
 }
--- BackEnd/api/proyectos/routes/routes.go
+++ BackEnd/api/proyectos/routes/routes.go
@@ -7,4 +7,7 @@ import (
 
 func rutas(ruta routes.TipoRuta) {
 	ruta("/CrearProyecto", controllers.CrearProyecto, "POST", "crea un nuevo proyecto")
+	ruta("/EliminarProyecto/{id}", controllers.EliminarProyecto, "DELETE", "eliminar un proyecto")
+	ruta("/ObtenerProyectos", controllers.ObtenerProyectos, "GET", "obtener proyectos")
+	ruta("/ModificarProyecto", controllers.ModificarProyecto, "PUT", "modificar proyecto")
 }
