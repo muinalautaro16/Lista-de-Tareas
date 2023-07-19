47794a6d0b9aac8c020a13cd609fe9a470489c31
fdrossi <fdrossi24@gmail.com>
2023-07-19 19:49:03 -0300

    Maqueteado del componente Etapas

---
+++ FrontEnd/src/app/Tareas/default/components/listaSubtarea.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import Textfield from '@smui/textfield';
+  import Button, { Label } from '@smui/button';  
+  import List, { Item, Meta } from '@smui/list';
+  import Checkbox from '@smui/checkbox';
+  import IconButton from '@smui/icon-button';
+
+  let changeEvent: CustomEvent<{ changedIndices: number[] }> | null;
+
+  let valueA = '';
+
+  let selected = [""];
+</script>
+
+
+<class class="columns margins">
+  <List
+  class="demo-list"
+  checkList
+  on:SMUIList:selectionChange={(event) => (changeEvent = event)}
+  style="display: flex;  flex-direction:column; align-items:flex-start"
+  >
+  <div style="display:flex">
+    <Item>
+      <Meta>
+        <Checkbox bind:group={selected} value="Etapa 1" />
+      </Meta>
+      <Label>Etapa 1</Label>      
+    </Item>
+    <IconButton class="material-icons" aria-label="Trash">delete</IconButton>
+  </div>
+  <div style="display:flex">
+    <Item>
+      <Meta>
+        <Checkbox bind:group={selected} value="Etapa 2" />
+      </Meta>
+      <Label>Etapa 1</Label>      
+    </Item>
+    <IconButton class="material-icons" aria-label="Trash">delete</IconButton>
+  </div>
+</List>
+
+<div  style="display: flex;">
+  <Button style="color:white; margin:2.5vh; margin-left:0vh;">
+    <Label style="font-size:5vh;">+</Label>
+  </Button>
+  
+  <Textfield bind:value={valueA} label="Crear Etapa"/>    
+</div>
+</class>
--- FrontEnd/src/app/Tareas/default/components/subTarea.svelte
+++ FrontEnd/src/app/Tareas/default/components/subTarea.svelte
@@ -1,52 +1,27 @@
-<script>
-    import Accordion, { Panel, Header, Content } from '@smui-extra/accordion';
-    import List, { Item, Separator, Text } from '@smui/list';
-    import Checkbox from '@smui/checkbox';
-    import Fab, { Icon } from '@smui/fab';
-    import Textfield from '@smui/textfield';
-    import FormField from '@smui/form-field';
-    import IconButton from '@smui/icon-button';
-    let ValueB = '';
-    let checked = false;
+<script lang="ts">
+	import ListaSubtarea from './listaSubtarea.svelte';
+  import Accordion, { Panel, Header, Content } from '@smui-extra/accordion';
+  import IconButton, { Icon } from '@smui/icon-button';
+  
+  let contadorTareas = 2;
+  let contadorTareasListas= 0;
+
+  let panel1Open = false;
 </script>
 
-<div style="color" class="accordion-container">
-    <Accordion>
-      <Panel>
-        <Header>Subtareas</Header>
-        <Content>
-          <div class="subtarea">
-            <List class="demo-list" nonInteractive>
-              
-              <FormField>
-                <Checkbox bind:checked />
-                <Item><Text>Subtarea 1</Text></Item>
-              </FormField>
-              <Separator />
-              
-            </List>
-            <IconButton class="material-icons" aria-label="Trash">delete</IconButton>
-          </div>
-          
-          <div>                 
-            <Fab mini color="secondary">
-                <Icon class="material-icons agregar">add</Icon>
-            </Fab>
-            <Textfield bind:value={ValueB} label="Crear Subtarea" />
-          </div>
-        </Content>
-      </Panel>
-    </Accordion>
-  </div>  
-
-<style>
-.subtarea {
-    display: flex;
-    align-items: center;
-}
-
-* :global(.demo-list) {
-    max-width: 240px;
-    
-  }
-</style>
+<div class="accordion-container">
+  <Accordion>
+    <Panel bind:open={panel1Open}>
+      <Header style="display:flex-direcction; flex-direction:row-reverse; background-color:#272822">
+        <IconButton slot="icon" toggle pressed={panel1Open}>
+          <Icon class="material-icons" on>expand_less</Icon>
+          <Icon class="material-icons">expand_more</Icon>
+        </IconButton>
+        Etapas - {contadorTareasListas} de {contadorTareas} Terminadas
+      </Header>
+      <Content>
+        <ListaSubtarea/>
+      </Content>
+    </Panel>
+  </Accordion>
+</div>
